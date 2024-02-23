8d5854e668fa513f5bc547694beb90a011a561e6
Мухаммаджон Набиев <nabievmuhammad1812@mail.ru>
2024-02-23 12:56:12 +0500

    the end practice form

---
+++ test-programs/error.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ошибка</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Ошибка</h1>
+    </div>
+</body>
+</html>
--- test-programs/js/app.js
+++ test-programs/js/app.js
@@ -1,7 +1,81 @@
-// Валидация номера телефона
-const phoneInput = document.getElementById('phone');
-phoneInput.addEventListener('input', function() {
-  const phoneNumber = phoneInput.value;
-  const isValid = /\+7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}/.test(phoneNumber);
-  phoneInput.setCustomValidity(isValid ? '' : 'Введите номер телефона в формате +7(777)777-77-77');
+let arrUsers = JSON.parse(localStorage.getItem('users')) || [];
+
+let selector = document.querySelector("#phone");
+let im = new Inputmask("+7(999) 999-99-99");
+im.mask(selector);
+
+let validation = new JustValidate("form");
+
+validation.addField("#name", [
+  {
+    rule: "required",
+    errorMessage: "Введите имя!"
+  },
+  {
+    rule: "minLength",
+    value: 2,
+    errorMessage: "Минимум 2 символа!"
+  }
+]).addField("#phone", [
+  {
+    validator: (value) => {
+      const phone = selector.inputmask.unmaskedvalue();
+      return Boolean(Number(phone) && phone.length > 0);
+    },
+    errorMessage: 'Введите телефон'
+  },
+  {
+    validator: (value) => {
+      const phone = selector.inputmask.unmaskedvalue();
+      return Boolean(Number(phone) && phone.length === 10);
+    },
+    errorMessage: 'Введите телефон полностью'
+  }
+]).onSuccess(async function () {
+  const nameInput = document.getElementById('name');
+  const phoneInput = document.getElementById('phone');
+
+  if (checkUserData(nameInput.value, arrUsers)) {
+    alert('Ошибка: Вы уже отправили заявку');
+  } else {
+    let data = {
+      name: nameInput.value,
+      tel: phoneInput.inputmask.unmaskedvalue(),
+    };
+
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'block';
+  
+    try {
+      let response = await fetch("order.php", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8"
+        }
+      });
+
+      let result = await response.text();
+      // Вызов ошибки для проверки error.php
+      // throw new Error('Пример ошибки');
+      saveUserData(nameInput.value, phoneInput.value);
+      window.location.href = 'thank_you.php';
+
+    } catch (error) {
+      window.location.href = 'error.php';
+    }
+
+    nameInput.value = '';
+    phoneInput.value = '';
+    spinner.style.display = 'none';
+  }
 });
+
+function saveUserData(name, phone) {
+  arrUsers.push({ name, phone });
+  localStorage.setItem('users', JSON.stringify(arrUsers));
+}
+
+function checkUserData(name, array) {
+  return array.some(user => user.name === name);
+}
